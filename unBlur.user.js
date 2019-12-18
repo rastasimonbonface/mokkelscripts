ebc9cb4adef562ef1c7eab00e44d4760c753fb5b
rastasimonbonface <rastasimonbonface@gmail.com>
2019-12-18 19:20:11 +0100

    icon

--- unBlur.user.js
+++ unBlur.user.js
@@ -3,10 +3,11 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.1
 // @description  try to take over the world!
+// @icon         https://forum.mannenzaken.nl/favicon.ico
 // @author       Rasta
 // @match        https://forum.mannenzaken.nl/*
-// @require https://code.jquery.com/jquery-3.2.1.min.js
-// @require https://code.jquery.com/jquery-migrate-3.0.0.min.js
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// @require      https://code.jquery.com/jquery-migrate-3.0.0.min.js
 // @grant        none
 // ==/UserScript==
 (function() {
